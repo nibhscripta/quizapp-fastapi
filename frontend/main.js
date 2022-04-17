d18fa359cebcc2bfd13580fd05c7c716fcbf5ff7
tux <nibhscripta@tutanota.com>
2022-04-16 22:19:29 -0500

    ✏️ form handling for joining quiz form

--- frontend/main.js
+++ frontend/main.js
@@ -17,3 +17,9 @@ const homeFormDiv = `
       </div>
 `;
 app.insertAdjacentHTML("afterend", homeFormDiv);
+// quiz form submit handling
+const quizForm = document.getElementById("start-quiz-form");
+quizForm.onsubmit = (e) => {
+  e.preventDefault();
+  const quizID = e.target.quiz_id.value;
+};
