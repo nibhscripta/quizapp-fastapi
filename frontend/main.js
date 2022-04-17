327b69365be5c36e67da412f95f0c1698471d7f9
tux <nibhscripta@tutanota.com>
2022-04-17 11:02:39 -0500

    ✏️ rework home page styles

--- frontend/main.js
+++ frontend/main.js
@@ -3,18 +3,7 @@ document.getElementById("noscript").remove();
 // render home form
 const app = document.getElementById("app");
 const homeFormDiv = `
-    <div class="home-main">
-        <div class="home-quiz-form">
-          <form id="start-quiz-form">
-            <input type="text" name="quiz_id" placeholder="Enter Quiz ID" />
-            <button type="submit">Start</button>
-          </form>
-          <div class="home-user-links">
-            <a href="./quiz">Create a quiz</a>
-            <a href="./login">Login</a>
-          </div>
-        </div>
-      </div>
+    
 `;
 app.insertAdjacentHTML("afterend", homeFormDiv);
 // quiz form submit handling
