8531088540684e76699f487dd9b7b27875d2372e
tux <nibhscripta@tutanota.com>
2022-04-16 23:08:54 -0500

    ✨ add assessment page

---
+++ frontend/assessment/assess.js
@@ -0,0 +1,62 @@
+// remove noscript warning
+document.getElementById("noscript").remove();
+
+// sample data
+const questions = [
+  {
+    id: 1,
+    question: "What is a limit?",
+  },
+  {
+    id: 2,
+    question: "What is a derivative?",
+  },
+  {
+    id: 3,
+    question: "What is an integral?",
+  },
+];
+const answers = [
+  [
+    {
+      id: 1,
+      answer: "A point on a curve.",
+    },
+    {
+      id: 2,
+      answer: "A tangent line",
+    },
+    {
+      id: 3,
+      answer: "The area under the curve.",
+    },
+  ],
+  [
+    {
+      id: 1,
+      answer: "A point on a curve.",
+    },
+    {
+      id: 2,
+      answer: "A tangent line",
+    },
+    {
+      id: 3,
+      answer: "The area under the curve.",
+    },
+  ],
+  [
+    {
+      id: 1,
+      answer: "A point on a curve.",
+    },
+    {
+      id: 2,
+      answer: "A tangent line",
+    },
+    {
+      id: 3,
+      answer: "The area under the curve.",
+    },
+  ],
+];
