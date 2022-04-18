64f3672a668447dfbd5df281e2b1058678af8350
tux <nibhscripta@tutanota.com>
2022-04-18 16:15:03 -0500

    ✨ get access token with axios

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -15,9 +15,31 @@ const loginFormDiv = `
 `;
 app.insertAdjacentHTML("beforeend", loginFormDiv);
 // handle login submission
+const apiUrl = "http://127.0.0.1:8000/";
+
 const loginForm = document.getElementById("login-form");
 loginForm.onsubmit = (e) => {
   e.preventDefault();
   const email = e.target.email.value;
   const password = e.target.password.value;
+  const data = new FormData();
+  data.append("username", email);
+  data.append("password", password);
+
+  const config = {
+    method: "post",
+    url: "/login",
+    baseURL: apiUrl,
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      const tokenData = JSON.stringify(response.data);
+      console.log(tokenData);
+    })
+    .catch(function (error) {
+      const errorData = error;
+      console.log(errorData);
+    });
 };
