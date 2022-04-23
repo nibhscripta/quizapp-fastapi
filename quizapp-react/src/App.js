b3684639725e02ef4df2bb34cf8850d0fcad4970
tux <nibhscripta@tutanota.com>
2022-04-22 22:15:40 -0500

    ✨ get auth token from frontend

--- quizapp-react/src/App.js
+++ quizapp-react/src/App.js
@@ -2,14 +2,17 @@ import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const apiUrl = "http://127.0.0.1:8000";
+
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login apiUrl={apiUrl} />} />
         </Routes>
       </Router>
     </div>
--- quizapp-react/src/pages/login/Login.js
+++ quizapp-react/src/pages/login/Login.js
@@ -2,11 +2,30 @@ import { Link } from "react-router-dom";
 
 import "./login.css";
 
-const Login = () => {
-  const login = (e) => {
+const Login = ({ apiUrl }) => {
+  const getJWT = async (username, passord, apiUrl) => {
+    let loginHeaders = new Headers();
+    let formdata = new FormData();
+    formdata.append("username", username);
+    formdata.append("password", passord);
+    var requestOptions = {
+      method: "POST",
+      headers: loginHeaders,
+      body: formdata,
+      redirect: "follow",
+    };
+    const res = await fetch(`${apiUrl}/login`, requestOptions);
+    const data = await res.json();
+    const authToken = `${data.token_type} ${data.access_token}`;
+    localStorage.setItem("authorization", JSON.stringify(authToken));
+    console.log(JSON.parse(localStorage.getItem("authorization")));
+  };
+
+  const login = async (e) => {
     e.preventDefault();
     const username = e.target.username.value;
     const password = e.target.password.value;
+    await getJWT(username, password, apiUrl);
     e.target.password.value = "";
   };
 
